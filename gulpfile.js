dec1fcef964eddb745c9f69eee8408fb1f1fc570
rkei (Keisuke Kato) <keisuke.kato@nulab-inc.com>
2017-03-05 13:09:26 +0900

    add postcss-stylish-dark-theme-gene

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,32 @@
 const gulp = require('gulp');
 const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const customProperties = require("postcss-custom-properties")
-const mqpacker = require('css-mqpacker');
-const csswring = require('csswring');
-const sourcemaps = require('gulp-sourcemaps');
-const stylelint = require('stylelint');
-const reporter = require('postcss-reporter');
-const cssnext = require('cssnext');
-const precss = require('precss');
 const concat = require('gulp-concat');
 const headerfooter = require('gulp-headerfooter');
-const colorNegator = require('./src/postcss/postcss-color-negator');
+const stylishDarkThemeGene = require('postcss-stylish-dark-theme-gene');
 
 // Src
 const css_src_header = './src/css/backlog-dark-header.css';
 const css_src_footer = './src/css/backlog-dark-footer.css';
 const css_src_dir = [
-      './history/01_09_2017/*.css'
+      './history/2017_03_05/*.css'
 ];
 
 // Dst
-const css_dst_dir = './dest/css';
+const css_dst_dir = './dist/css';
 const css_dst_file = 'backlog-dark.css';
 
 // PostCSS
 gulp.task('css', function () {
     const processors = [
-        stylelint()
-        , reporter({clearMessages: true})
-        , customProperties()
-        , autoprefixer({browsers: ['last 2 version']})
-        , colorNegator
-        , cssnext
-        , precss
-        , mqpacker
-        , csswring
+        stylishDarkThemeGene
     ];
     return gulp.src(css_src_dir)
-        .pipe(sourcemaps.init())
         .pipe(postcss(processors))
         .pipe(concat(css_dst_file))
         .pipe(headerfooter.header(css_src_header))
         .pipe(headerfooter.footer(css_src_footer))
-        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(css_dst_dir));
 });
-
 
 // Watch
 gulp.task('watch', function () {
@@ -61,4 +41,3 @@ gulp.task('default', [
     'css'
     , 'watch'
 ]);
-
